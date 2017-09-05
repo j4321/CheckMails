0c4ea839f9f660b2c9225acb37502ac1ba79c34b
Juliette Monsel <j_4321@protonmail.com>
2017-09-05 17:13:35 +0200

    Fix typo and update changelog

--- checkmailslib/check.py
+++ checkmailslib/check.py
@@ -267,7 +267,7 @@ class CheckMails(Tk):
                 logging.exception("Incorrect login or password for %(mailbox)s" % {"mailbox": box})
             else:
                 # try to reconnect
-                logging.exception('%s: %s:' (type(e), str(e)))
+                logging.exception('%s: %s:' % (type(e), str(e)))
                 run(["notify-send", "-i", "dialog-error", str(type(e)),
                      str(e)])
                 self.logout(box, reconnect=True)
